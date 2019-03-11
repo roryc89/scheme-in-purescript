5430cb2588323bf78cbe8755d0158e8408d3d3c5
roryc89 <roryc89@googlemail.com>
2019-03-11 19:37:09 +0000

    evaluation part 2 done

--- src/Eval.purs
+++ src/Eval.purs
@@ -2,9 +2,10 @@ module Eval where
 
 import Prelude
 
-import Control.Monad.Error.Class (throwError)
-import Data.Either (Either)
-import Data.List (List(..), (:))
+import Control.Alt ((<|>))
+import Control.Monad.Error.Class (catchError, throwError)
+import Data.Either (Either(..))
+import Data.List (List(..), all, length, zip, (:))
 import Data.Maybe (maybe)
 import Data.NonEmpty (NonEmpty(..), foldl1)
 import Data.Traversable (traverse)
@@ -21,8 +22,9 @@ eval (List (Atom "if" : pred : conseq : alt : Nil)) =
      do
        result <- eval pred
        case result of
+         Bool true  -> eval conseq
          Bool false -> eval alt
-         otherwise  -> eval conseq
+         _ -> throwError $ TypeMismatch "Boolean" result
 
 eval (List (Atom func : args)) = traverse eval args >>= applyLisp func
 eval badForm = throwError $ BadSpecialForm "Unrecognized special form" badForm
@@ -54,6 +56,12 @@ primitives =
   , Tuple "string>?" (strBoolBinop (>))
   , Tuple "string<=?" (strBoolBinop (<=))
   , Tuple "string>=?" (strBoolBinop (>=))
+  , Tuple "car" car
+  , Tuple "cdr" cdr
+  , Tuple "cons" cons
+  , Tuple "eq?" eqv
+  , Tuple "eqv?" eqv
+  , Tuple "equal?" equal
   ]
 
 numericBinop :: (Number -> Number -> Number) -> List LispVal -> ThrowsError LispVal
@@ -101,3 +109,85 @@ unpackStr notString  = throwError $ TypeMismatch "string" notString
 unpackBool :: LispVal -> ThrowsError Boolean
 unpackBool (Bool b) = pure b
 unpackBool notBool  = throwError $ TypeMismatch "boolean" notBool
+
+-- PRIMITIVE FUNCTIONS
+
+car :: List LispVal -> ThrowsError LispVal
+car ((List (x : xs)) : Nil) =
+  pure x
+car ((DottedList (x : xs) _) : Nil) =
+  pure x
+car (badArg : Nil) =
+  throwError $ TypeMismatch "pair" badArg
+car badArgList = throwError $ NumArgs 1 badArgList
+
+cdr :: List LispVal -> ThrowsError LispVal
+cdr ((List (x : xs)) : Nil) = pure $ List xs
+cdr ((DottedList (_ : Nil) x) : Nil) =  pure x
+cdr ((DottedList (_ : xs) x) : Nil) = pure $ DottedList xs x
+cdr (badArg : Nil) =
+  throwError $ TypeMismatch "pair" badArg
+cdr badArgList = throwError $ NumArgs 1 badArgList
+
+
+cons :: List LispVal -> ThrowsError LispVal
+cons (x1 : List Nil : Nil) = pure $ List (x1 : Nil)
+cons (x : List xs : Nil) = pure $ List $ x : xs
+cons (x : DottedList xs xlast : Nil) = pure $ DottedList (x : xs) xlast
+cons (x1 : x2 : Nil) = pure $ DottedList (x1 : Nil) x2
+cons badArgList = throwError $ NumArgs 2 badArgList
+
+eqv :: List LispVal -> ThrowsError LispVal
+eqv ((Bool arg1) : (Bool arg2) : Nil) =
+  pure $ Bool $ arg1 == arg2
+
+eqv ((Number arg1) : (Number arg2) : Nil) =
+  pure $ Bool $ arg1 == arg2
+
+eqv ((String arg1) : (String arg2) : Nil) =
+  pure $ Bool $ arg1 == arg2
+
+eqv ((Atom arg1) : (Atom arg2) : Nil) =
+  pure $ Bool $ arg1 == arg2
+
+eqv ((DottedList xs x) : (DottedList ys y) : Nil) =
+  eqv $
+    List (xs <> (pure x))
+    : (List (ys <> (pure y)))
+    : Nil
+
+eqv ((List arg1) : (List arg2) : Nil) =
+  let
+    eqvPair (Tuple x1 x2) = case eqv (x1 : x2 : Nil) of
+      Left err -> false
+      Right (Bool val) -> val
+      Right _ -> false
+  in
+  pure $ Bool $ (length arg1 == length arg2) && (all eqvPair $ zip arg1 arg2)
+
+eqv (_ : _ : Nil)                          = pure $ Bool false
+eqv badArgList                             = throwError $ NumArgs 2 badArgList
+
+
+unpackEquals :: forall a. Eq a => LispVal -> LispVal -> (LispVal -> ThrowsError a) -> ThrowsError Boolean
+unpackEquals arg1 arg2 unpacker = do
+  unpacked1 <- unpacker arg1
+  unpacked2 <- unpacker arg2
+  pure $ unpacked1 == unpacked2
+  `catchError` (const $ pure false)
+
+
+equal :: List LispVal -> ThrowsError LispVal
+equal (arg1 : arg2 : Nil) = do
+  primitiveEquals <-
+    (unpackEquals arg1 arg2 unpackNum)
+    <|> (unpackEquals arg1 arg2 unpackStr)
+    <|> (unpackEquals arg1 arg2 unpackBool)
+
+  eqvEquals <- eqv (arg1 : arg2 : Nil)
+
+  pure $ case eqvEquals of
+    Bool b -> Bool $ (primitiveEquals || b)
+    _ -> Bool primitiveEquals
+
+equal badArgList = throwError $ NumArgs 2 badArgList
